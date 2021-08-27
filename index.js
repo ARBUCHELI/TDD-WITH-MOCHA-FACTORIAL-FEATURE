8a465bc7676391c4e5392ad48503dba61c5651fd
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-08-27 17:54:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const Calculate = {
+  
+    factorial(input) {
+      if(input === 0) {
+        return 1;
+      }
+      for (let i = input - 1; i >= 1; i--) {
+        input*= i;
+      }
+      return input;
+    }
+  }
+  
+  module.exports = Calculate;
